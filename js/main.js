9be35ade20a148e7c34a69d73f46131addfb377e
Ethan Toro <ethan.j.toro@gmail.com>
2019-04-08 12:25:28 -0400

    9.3.0
    1 Page Design

--- js/main.js
+++ js/main.js
@@ -1,53 +1,20 @@
-/* Nav Bar */
+var slideIndex = 0;
+// Slide Show
 
+showSlides();
 
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
-
-
-
-
-  /* Portfolio */
-
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var imgs = document.getElementsByClassName('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var displayImage = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
-
-for (var i = 0; i< imgs.length; i++) {
-  imgs[i].onclick = displayImage;
-}
-
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
 
 // Smooth Scrolling
